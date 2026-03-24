yngrdyn/kibana

commit 6dbf95123d46eb7c4a95076e85daea6d275a0435

[Entity Store] Extract first_seen and last_seen from @timestamp (#258495)
## Summary

Modifies `entity.lifecycle.first_seen` and `entity.lifecycle.last_seen`
to represent true state by holding onto first and latest timestamps
respectively.
**EDIT:** `entity.lifecycle.last_activity` should stay the same,
`last_seen` added.

Closes https://github.com/elastic/kibana/issues/255084

### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/solutions/security/plugins/entity_store/common/domain/definitions/common_fields.ts
@@ -7,7 +7,7 @@
 
 import type { Condition } from '@kbn/streamlang';
 import type { EntityType, EntityField } from './entity_schema';
-import { collectValues, newestValue } from './field_retention_operations';
+import { collectValues, newestValue, oldestValue } from './field_retention_operations';
 
 export const ENTITY_ID_FIELD = 'entity.id';
 // Copied from x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/entity_store/entity_definitions/entity_descriptions/common.ts
@@ -84,13 +84,18 @@ export const getEntityFieldsDescriptions = (rootField?: EntityType) => {
     }),
 
     // LIFECYCLE ------------------------------------------------------------
-    newestValue({
-      source: `${prefix}.lifecycle.first_seen`,
+    oldestValue({
+      source: '@timestamp',
       destination: 'entity.lifecycle.first_seen',
       mapping: { type: 'date' },
     }),
     newestValue({
-      source: `${prefix}.lifecycle.last_activity`,
+      source: '@timestamp',
+      destination: 'entity.lifecycle.last_seen',
+      mapping: { type: 'date' },
+    }),
+    newestValue({
+      source: '${prefix}.lifecycle.last_activity',
       destination: 'entity.lifecycle.last_activity',
       mapping: { type: 'date' },
     }),

x-pack/solutions/security/plugins/entity_store/server/domain/logs_extraction/__snapshots__/ccs_logs_extraction_query_builder.test.ts.snap
@@ -72,8 +72,9 @@ FROM remote:metrics-*
  entity.attributes.asset = LAST(TO_BOOLEAN(host.entity.attributes.asset), @timestamp) WHERE host.entity.attributes.asset IS NOT NULL,
  entity.attributes.managed = LAST(TO_BOOLEAN(host.entity.attributes.managed), @timestamp) WHERE host.entity.attributes.managed IS NOT NULL,
  entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(host.entity.attributes.mfa_enabled), @timestamp) WHERE host.entity.attributes.mfa_enabled IS NOT NULL,
- entity.lifecycle.first_seen = LAST(TO_DATETIME(host.entity.lifecycle.first_seen), @timestamp) WHERE host.entity.lifecycle.first_seen IS NOT NULL,
- entity.lifecycle.last_activity = LAST(TO_DATETIME(host.entity.lifecycle.last_activity), @timestamp) WHERE host.entity.lifecycle.last_activity IS NOT NULL,
+ entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
+ entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
+ entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
  entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.rule_names), 100)) WHERE host.entity.behaviors.rule_names IS NOT NULL,
  entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.anomaly_job_ids), 100)) WHERE host.entity.behaviors.anomaly_job_ids IS NOT NULL,
  entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(host.entity.relationships.communicates_with), 10)) WHERE host.entity.relationships.communicates_with IS NOT NULL,
@@ -154,8 +155,9 @@ FROM remote:logs-*
  entity.attributes.asset = LAST(TO_BOOLEAN(user.entity.attributes.asset), @timestamp) WHERE user.entity.attributes.asset IS NOT NULL,
  entity.attributes.managed = LAST(TO_BOOLEAN(user.entity.attributes.managed), @timestamp) WHERE user.entity.attributes.managed IS NOT NULL,
  entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(user.entity.attributes.mfa_enabled), @timestamp) WHERE user.entity.attributes.mfa_enabled IS NOT NULL,
- entity.lifecycle.first_seen = LAST(TO_DATETIME(user.entity.lifecycle.first_seen), @timestamp) WHERE user.entity.lifecycle.first_seen IS NOT NULL,
- entity.lifecycle.last_activity = LAST(TO_DATETIME(user.entity.lifecycle.last_activity), @timestamp) WHERE user.entity.lifecycle.last_activity IS NOT NULL,
+ entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
+ entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
+ entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
  entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(user.entity.behaviors.rule_names), 100)) WHERE user.entity.behaviors.rule_names IS NOT NULL,
  entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(user.entity.behaviors.anomaly_job_ids), 100)) WHERE user.entity.behaviors.anomaly_job_ids IS NOT NULL,
  entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(user.entity.relationships.communicates_with), 10)) WHERE user.entity.relationships.communicates_with IS NOT NULL,
@@ -209,8 +211,9 @@ FROM remote_cluster:logs-*
  entity.attributes.asset = LAST(TO_BOOLEAN(entity.attributes.asset), @timestamp) WHERE entity.attributes.asset IS NOT NULL,
  entity.attributes.managed = LAST(TO_BOOLEAN(entity.attributes.managed), @timestamp) WHERE entity.attributes.managed IS NOT NULL,
  entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(entity.attributes.mfa_enabled), @timestamp) WHERE entity.attributes.mfa_enabled IS NOT NULL,
- entity.lifecycle.first_seen = LAST(TO_DATETIME(entity.lifecycle.first_seen), @timestamp) WHERE entity.lifecycle.first_seen IS NOT NULL,
- entity.lifecycle.last_activity = LAST(TO_DATETIME(entity.lifecycle.last_activity), @timestamp) WHERE entity.lifecycle.last_activity IS NOT NULL,
+ entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
+ entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
+ entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
  entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(entity.behaviors.rule_names), 100)) WHERE entity.behaviors.rule_names IS NOT NULL,
  entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(entity.behaviors.anomaly_job_ids), 100)) WHERE entity.behaviors.anomaly_job_ids IS NOT NULL,
  entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(entity.relationships.communicates_with), 10)) WHERE entity.relationships.communicates_with IS NOT NULL,

commit 119217f76b87b7792355f353ea1b6da0b50e3da5

[Entity Store] Fix string template bug from #258495 (#259057)
Fixes a string templating bug introduced by
https://github.com/elastic/kibana/pull/258495
- Use the validateQuery from @kbn/esql-language to va;idate the ESQL
queries we generate.

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/solutions/security/plugins/entity_store/common/domain/definitions/common_fields.ts
@@ -95,7 +95,7 @@ export const getEntityFieldsDescriptions = (rootField?: EntityType) => {
       mapping: { type: 'date' },
     }),
     newestValue({
-      source: '${prefix}.lifecycle.last_activity',
+      source: `${prefix}.lifecycle.last_activity`,
       destination: 'entity.lifecycle.last_activity',
       mapping: { type: 'date' },
     }),

x-pack/solutions/security/plugins/entity_store/server/domain/logs_extraction/__snapshots__/ccs_logs_extraction_query_builder.test.ts.snap
@@ -74,7 +74,7 @@ FROM remote:metrics-*
  entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(host.entity.attributes.mfa_enabled), @timestamp) WHERE host.entity.attributes.mfa_enabled IS NOT NULL,
  entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ entity.lifecycle.last_activity = LAST(TO_DATETIME(host.entity.lifecycle.last_activity), @timestamp) WHERE host.entity.lifecycle.last_activity IS NOT NULL,
  entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.rule_names), 100)) WHERE host.entity.behaviors.rule_names IS NOT NULL,
  entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.anomaly_job_ids), 100)) WHERE host.entity.behaviors.anomaly_job_ids IS NOT NULL,
  entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(host.entity.relationships.communicates_with), 10)) WHERE host.entity.relationships.communicates_with IS NOT NULL,
@@ -157,7 +157,7 @@ FROM remote:logs-*
  entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(user.entity.attributes.mfa_enabled), @timestamp) WHERE user.entity.attributes.mfa_enabled IS NOT NULL,
  entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ entity.lifecycle.last_activity = LAST(TO_DATETIME(user.entity.lifecycle.last_activity), @timestamp) WHERE user.entity.lifecycle.last_activity IS NOT NULL,
  entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(user.entity.behaviors.rule_names), 100)) WHERE user.entity.behaviors.rule_names IS NOT NULL,
  entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(user.entity.behaviors.anomaly_job_ids), 100)) WHERE user.entity.behaviors.anomaly_job_ids IS NOT NULL,
  entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(user.entity.relationships.communicates_with), 10)) WHERE user.entity.relationships.communicates_with IS NOT NULL,
@@ -213,7 +213,7 @@ FROM remote_cluster:logs-*
  entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(entity.attributes.mfa_enabled), @timestamp) WHERE entity.attributes.mfa_enabled IS NOT NULL,
  entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ entity.lifecycle.last_activity = LAST(TO_DATETIME(entity.lifecycle.last_activity), @timestamp) WHERE entity.lifecycle.last_activity IS NOT NULL,
  entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(entity.behaviors.rule_names), 100)) WHERE entity.behaviors.rule_names IS NOT NULL,
  entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(entity.behaviors.anomaly_job_ids), 100)) WHERE entity.behaviors.anomaly_job_ids IS NOT NULL,
  entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(entity.relationships.communicates_with), 10)) WHERE entity.relationships.communicates_with IS NOT NULL,

x-pack/solutions/security/plugins/entity_store/server/domain/logs_extraction/__snapshots__/logs_extraction_query_builder.test.ts.snap
@@ -23,7 +23,7 @@ exports[`buildLogsExtractionEsqlQuery generates the expected query for generic e
  recent.entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(entity.attributes.mfa_enabled), @timestamp) WHERE entity.attributes.mfa_enabled IS NOT NULL,
  recent.entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  recent.entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(entity.lifecycle.last_activity), @timestamp) WHERE entity.lifecycle.last_activity IS NOT NULL,
  recent.entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(entity.behaviors.rule_names), 100)) WHERE entity.behaviors.rule_names IS NOT NULL,
  recent.entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(entity.behaviors.anomaly_job_ids), 100)) WHERE entity.behaviors.anomaly_job_ids IS NOT NULL,
  recent.entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(entity.relationships.communicates_with), 10)) WHERE entity.relationships.communicates_with IS NOT NULL,
@@ -233,7 +233,7 @@ exports[`buildLogsExtractionEsqlQuery generates the expected query for host enti
  recent.entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(host.entity.attributes.mfa_enabled), @timestamp) WHERE host.entity.attributes.mfa_enabled IS NOT NULL,
  recent.entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  recent.entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(host.entity.lifecycle.last_activity), @timestamp) WHERE host.entity.lifecycle.last_activity IS NOT NULL,
  recent.entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.rule_names), 100)) WHERE host.entity.behaviors.rule_names IS NOT NULL,
  recent.entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.anomaly_job_ids), 100)) WHERE host.entity.behaviors.anomaly_job_ids IS NOT NULL,
  recent.entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(host.entity.relationships.communicates_with), 10)) WHERE host.entity.relationships.communicates_with IS NOT NULL,
@@ -414,7 +414,7 @@ exports[`buildLogsExtractionEsqlQuery generates the expected query for host with
  recent.entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(host.entity.attributes.mfa_enabled), @timestamp) WHERE host.entity.attributes.mfa_enabled IS NOT NULL,
  recent.entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  recent.entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(host.entity.lifecycle.last_activity), @timestamp) WHERE host.entity.lifecycle.last_activity IS NOT NULL,
  recent.entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.rule_names), 100)) WHERE host.entity.behaviors.rule_names IS NOT NULL,
  recent.entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.anomaly_job_ids), 100)) WHERE host.entity.behaviors.anomaly_job_ids IS NOT NULL,
  recent.entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(host.entity.relationships.communicates_with), 10)) WHERE host.entity.relationships.communicates_with IS NOT NULL,
@@ -598,7 +598,7 @@ exports[`buildLogsExtractionEsqlQuery generates the expected query for host with
  recent.entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(host.entity.attributes.mfa_enabled), @timestamp) WHERE host.entity.attributes.mfa_enabled IS NOT NULL,
  recent.entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  recent.entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(host.entity.lifecycle.last_activity), @timestamp) WHERE host.entity.lifecycle.last_activity IS NOT NULL,
  recent.entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.rule_names), 100)) WHERE host.entity.behaviors.rule_names IS NOT NULL,
  recent.entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(host.entity.behaviors.anomaly_job_ids), 100)) WHERE host.entity.behaviors.anomaly_job_ids IS NOT NULL,
  recent.entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(host.entity.relationships.communicates_with), 10)) WHERE host.entity.relationships.communicates_with IS NOT NULL,
@@ -756,7 +756,7 @@ exports[`buildLogsExtractionEsqlQuery generates the expected query for service e
  recent.entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(service.entity.attributes.mfa_enabled), @timestamp) WHERE service.entity.attributes.mfa_enabled IS NOT NULL,
  recent.entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  recent.entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(service.entity.lifecycle.last_activity), @timestamp) WHERE service.entity.lifecycle.last_activity IS NOT NULL,
  recent.entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(service.entity.behaviors.rule_names), 100)) WHERE service.entity.behaviors.rule_names IS NOT NULL,
  recent.entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(service.entity.behaviors.anomaly_job_ids), 100)) WHERE service.entity.behaviors.anomaly_job_ids IS NOT NULL,
  recent.entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(service.entity.relationships.communicates_with), 10)) WHERE service.entity.relationships.communicates_with IS NOT NULL,
@@ -899,7 +899,7 @@ exports[`buildLogsExtractionEsqlQuery generates the expected query for user enti
  recent.entity.attributes.mfa_enabled = LAST(TO_BOOLEAN(user.entity.attributes.mfa_enabled), @timestamp) WHERE user.entity.attributes.mfa_enabled IS NOT NULL,
  recent.entity.lifecycle.first_seen = FIRST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
  recent.entity.lifecycle.last_seen = LAST(TO_DATETIME(@timestamp), @timestamp) WHERE @timestamp IS NOT NULL,
- recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(\${prefix}.lifecycle.last_activity), @timestamp) WHERE \${prefix}.lifecycle.last_activity IS NOT NULL,
+ recent.entity.lifecycle.last_activity = LAST(TO_DATETIME(user.entity.lifecycle.last_activity), @timestamp) WHERE user.entity.lifecycle.last_activity IS NOT NULL,
  recent.entity.behaviors.rule_names = MV_DEDUPE(TOP(TO_STRING(user.entity.behaviors.rule_names), 100)) WHERE user.entity.behaviors.rule_names IS NOT NULL,
  recent.entity.behaviors.anomaly_job_ids = MV_DEDUPE(TOP(TO_STRING(user.entity.behaviors.anomaly_job_ids), 100)) WHERE user.entity.behaviors.anomaly_job_ids IS NOT NULL,
  recent.entity.relationships.communicates_with = MV_DEDUPE(TOP(TO_STRING(user.entity.relationships.communicates_with), 10)) WHERE user.entity.relationships.communicates_with IS NOT NULL,

x-pack/solutions/security/plugins/entity_store/common/domain/definitions/common_fields.test.ts
@@ -1,0 +1,41 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { getCommonFieldDescriptions, getEntityFieldsDescriptions } from './common_fields';
+import { ALL_ENTITY_TYPES } from './entity_schema';
+
+const UNTEMPLATED_VAR = /\$\{[^}]*\}/;
+
+describe('common_fields', () => {
+  describe('getCommonFieldDescriptions', () => {
+    it.each(ALL_ENTITY_TYPES)(
+      'should not contain untemplated variables for ecsField=%s',
+      (entityType) => {
+        const fields = getCommonFieldDescriptions(entityType);
+
+        for (const field of fields) {
+          expect(field.source).not.toMatch(UNTEMPLATED_VAR);
+          expect(field.destination).not.toMatch(UNTEMPLATED_VAR);
+        }
+      }
+    );
+  });
+
+  describe('getEntityFieldsDescriptions', () => {
+    it.each(ALL_ENTITY_TYPES)(
+      'should not contain untemplated variables for rootField=%s',
+      (rootField) => {
+        const fields = getEntityFieldsDescriptions(rootField);
+
+        for (const field of fields) {
+          expect(field.source).not.toMatch(UNTEMPLATED_VAR);
+          expect(field.destination).not.toMatch(UNTEMPLATED_VAR);
+        }
+      }
+    );
+  });
+});