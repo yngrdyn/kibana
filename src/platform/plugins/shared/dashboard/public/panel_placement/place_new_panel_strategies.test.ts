yngrdyn/kibana

commit 3dd9a8ab60fe4fb2f45d45981e489e21c196f66d

[Dashboard] [Controls] Add above top section when unpinning  (#273089)
Closes https://github.com/elastic/kibana/issues/264029

## Summary

This PR ensures that, when controls are unpinned from the top of the
dashboard, they are added **above** any sections in the dashboard rather
than below.


**Before**


https://github.com/user-attachments/assets/ace4b7b5-0439-4122-9bf3-20d44d7c0a6f


**After**


https://github.com/user-attachments/assets/69a033c1-87d8-43a8-9c26-89779e2bab2b


---

It does this by adjusting the `placeAtTop` algorithm for adding a new
panel.

Before, we **only** considered pushing **panels** down, not sections;
while this worked in most cases (since moving panels down would
automatically force sections down in our collision detection), this
**broke** when a section was at the top of a dashboard. Because sections
were ignored, the control was placed "on top of" (both with `y=0`) the
section which, due to our collision resolution, would push the control
below the section. This PR adjusts this behaviour so that it pushes all
**sections** down by the height of the new panel too, not just panels.

In order to make this work, I made some adjustments to how
`runPanelPlacementStrategy` works as a whole. Now, we take the **entire
layout** as an argument and return the **entire updated layout** rather
than the grid of the new panel and `otherPanels`. This explains the
changes to the other placement strategy cases - I did not change the
behaviour of these, I just had to adjust their logic a bit to
accommodate this new design.


### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

src/platform/plugins/shared/dashboard/public/panel_placement/place_new_panel_strategies.ts
@@ -16,16 +16,26 @@ import type { DashboardLayoutPanel } from '../dashboard_api/layout_manager';
 
 export const runPanelPlacementStrategy = (
   strategy: PlacementStrategy,
-  { width, height, currentPanels, sectionId, beside }: PanelPlacementProps
+  { newPanel, currentLayout, beside }: PanelPlacementProps
 ): PanelPlacementReturn => {
   let targetPanel: DashboardLayoutPanel | undefined;
+  const { panels: currentPanels, sections: currentSections } = currentLayout;
+  const { w: width, h: height, sectionId } = newPanel.grid;
   if (beside) {
     targetPanel = currentPanels[beside];
   }
   switch (strategy) {
     case PlacementStrategy.placeAtTop:
       const otherPanels = { ...currentPanels };
-      for (const [id, panel] of Object.entries(currentPanels)) {
+      const otherSections = { ...currentSections };
+
+      // push all sections down
+      for (const [id, section] of Object.entries({ ...currentSections })) {
+        otherSections[id] = { ...section, grid: { y: section.grid.y + height } };
+      }
+
+      // push relative panels down
+      for (const [id, panel] of Object.entries({ ...currentPanels })) {
         // only consider collisions with panels in the same section
         if (!sectionId || panel.grid.sectionId === sectionId) {
           const { grid, ...currentPanel } = cloneDeep(panel);
@@ -36,11 +46,25 @@ export const runPanelPlacementStrategy = (
           }
         }
       }
+
       return {
-        newPanelPlacement: targetPanel
-          ? { x: targetPanel.grid.x, y: targetPanel.grid.y, w: width, h: height }
-          : { x: 0, y: 0, w: width, h: height },
-        otherPanels,
+        ...currentLayout,
+        sections: otherSections,
+        panels: {
+          ...otherPanels,
+          [newPanel.uuid]: {
+            type: newPanel.type,
+            grid: targetPanel
+              ? {
+                  ...newPanel.grid,
+                  x: targetPanel.grid.x,
+                  y: targetPanel.grid.y,
+                  w: width,
+                  h: height,
+                }
+              : { ...newPanel.grid, x: 0, y: 0, w: width, h: height },
+          },
+        },
       };
 
     case PlacementStrategy.findTopLeftMostOpenSpace:
@@ -57,8 +81,10 @@ export const runPanelPlacementStrategy = (
       // Handle case of empty grid.
       if (maxY < 0) {
         return {
-          newPanelPlacement: { x: 0, y: 0, w: width, h: height },
-          otherPanels: currentPanels,
+          ...currentLayout,
+          panels: {
+            [newPanel.uuid]: { type: newPanel.type, grid: { x: 0, y: 0, w: width, h: height } },
+          },
         };
       }
 
@@ -101,9 +127,13 @@ export const runPanelPlacementStrategy = (
 
                 if (spaceIsEmpty && fitsPanelWidth && fitsPanelHeight) {
                   // Found space
+
                   return {
-                    newPanelPlacement: { x, y, w: width, h: height },
-                    otherPanels: currentPanels,
+                    ...currentLayout,
+                    panels: {
+                      ...currentPanels,
+                      [newPanel.uuid]: { type: newPanel.type, grid: { x, y, w: width, h: height } },
+                    },
                   };
                 } else if (grid[h][w] === 1) {
                   // x, y spot doesn't work, break.
@@ -115,13 +145,11 @@ export const runPanelPlacementStrategy = (
         }
       }
       return {
-        newPanelPlacement: {
-          x: 0,
-          y: maxY,
-          w: width,
-          h: height,
+        ...currentLayout,
+        panels: {
+          ...currentPanels,
+          [newPanel.uuid]: { type: newPanel.type, grid: { x: 0, y: maxY, w: width, h: height } },
         },
-        otherPanels: currentPanels,
       };
     default:
       throw new Error(

src/platform/plugins/shared/dashboard/public/panel_placement/place_new_panel_strategies.test.ts
@@ -15,78 +15,79 @@ import { runPanelPlacementStrategy } from './place_new_panel_strategies';
 describe('new panel placement strategies', () => {
   describe('place at top', () => {
     it('no other panels', () => {
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.placeAtTop,
-        { width: 6, height: 6, currentPanels: {} }
-      );
-      expect(newPanelPlacement).toEqual({
-        x: 0,
-        y: 0,
-        w: 6,
-        h: 6,
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.placeAtTop, {
+        currentLayout: { panels: {}, sections: {}, pinnedPanels: {} },
+        newPanel: { uuid: 'newPanel', type: 'panelType', grid: { w: 6, h: 6 } },
       });
-      expect(otherPanels).toEqual({});
+      expect(newLayout).toEqual({
+        sections: {},
+        pinnedPanels: {},
+        panels: { newPanel: { type: 'panelType', grid: { x: 0, y: 0, w: 6, h: 6 } } },
+      });
     });
 
     it('push other panels down', () => {
       const panels = getMockLayout().panels;
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.placeAtTop,
-        { width: 6, height: 6, currentPanels: panels }
-      );
-      expect(newPanelPlacement).toEqual({
-        x: 0,
-        y: 0,
-        w: 6,
-        h: 6,
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.placeAtTop, {
+        currentLayout: { panels, sections: {}, pinnedPanels: {} },
+        newPanel: { uuid: 'newPanel', type: 'panelType', grid: { w: 6, h: 6 } },
       });
-      expect(otherPanels).toEqual(
-        Object.keys(panels).reduce((prev, panelId) => {
-          const originalgrid = panels[panelId].grid;
-          return {
-            ...prev,
-            [panelId]: {
-              ...panels[panelId],
-              grid: {
-                ...originalgrid,
-                y: originalgrid.y + 6, // panel was pushed down by height of new panel
-              },
+      const expectedPanels = Object.keys(newLayout.panels).reduce((prev, panelId) => {
+        if (panelId === 'newPanel') {
+          return { ...prev, newPanel: { type: 'panelType', grid: { x: 0, y: 0, w: 6, h: 6 } } };
+        }
+        const originalgrid = panels[panelId].grid;
+        return {
+          ...prev,
+          [panelId]: {
+            ...panels[panelId],
+            grid: {
+              ...originalgrid,
+              y: originalgrid.y + 6, // panel was pushed down by height of new panel
             },
-          };
-        }, {})
-      );
+          },
+        };
+      }, {});
+      expect(newLayout.panels).toEqual(expectedPanels);
     });
 
     it('ignores panels in other sections', () => {
-      const panels = getMockLayoutWithSections().panels;
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.placeAtTop,
-        { width: 6, height: 6, currentPanels: panels, sectionId: 'section1' }
-      );
-      expect(newPanelPlacement).toEqual({
-        x: 0,
-        y: 0,
-        w: 6,
-        h: 6,
+      const mockedLayout = getMockLayoutWithSections();
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.placeAtTop, {
+        currentLayout: mockedLayout,
+        newPanel: {
+          uuid: 'newPanel',
+          type: 'panelType',
+          grid: { w: 6, h: 6, sectionId: 'section1' },
+        },
       });
-      expect(otherPanels).toEqual(
-        Object.keys(panels).reduce((prev, panelId) => {
-          const originalgrid = panels[panelId].grid;
+
+      const expectedPanels = Object.keys(newLayout.panels).reduce((prev, panelId) => {
+        if (panelId === 'newPanel') {
           return {
             ...prev,
-            [panelId]: {
-              ...panels[panelId],
-              grid: {
-                ...originalgrid,
-                // only panels in the targetted section should get pushed down
-                ...(originalgrid.sectionId === 'section1' && {
-                  y: originalgrid.y + 6,
-                }),
-              },
+            newPanel: {
+              type: 'panelType',
+              grid: { sectionId: 'section1', x: 0, y: 0, w: 6, h: 6 },
             },
           };
-        }, {})
-      );
+        }
+        const originalgrid = mockedLayout.panels[panelId].grid;
+        return {
+          ...prev,
+          [panelId]: {
+            ...mockedLayout.panels[panelId],
+            grid: {
+              ...originalgrid,
+              // only panels in the targetted section should get pushed down
+              ...(originalgrid.sectionId === 'section1' && {
+                y: originalgrid.y + 6,
+              }),
+            },
+          },
+        };
+      }, {});
+      expect(newLayout.panels).toEqual(expectedPanels);
     });
 
     it('place panel above another panel', () => {
@@ -97,40 +98,43 @@ describe('new panel placement strategies', () => {
           type: 'testPanelType',
         },
       };
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.placeAtTop,
-        { width: 6, height: 6, currentPanels: panels, beside: '3' }
-      );
-      expect(newPanelPlacement).toEqual({
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.placeAtTop, {
+        currentLayout: { panels, sections: {}, pinnedPanels: {} },
+        newPanel: {
+          uuid: 'newPanel',
+          type: 'panelType',
+          grid: { w: 6, h: 6 },
+        },
+        beside: '3',
+      });
+      expect(newLayout.panels.newPanel.grid).toEqual({
         x: 6,
         y: 6, // place below panel 2 but above panel 3
         w: 6,
         h: 6,
       });
       // panels 1 and 2 shouldn't move
-      expect(otherPanels['1'].grid).toEqual(panels['1'].grid);
-      expect(otherPanels['2'].grid).toEqual(panels['2'].grid);
+      expect(newLayout.panels['1'].grid).toEqual(panels['1'].grid);
+      expect(newLayout.panels['2'].grid).toEqual(panels['2'].grid);
       // panel 3 should have been pushed down by the height of the new panel
-      expect(otherPanels['3'].grid).toEqual({
+      expect(newLayout.panels['3'].grid).toEqual({
         ...panels['3'].grid,
-        y: panels['3'].grid.y + newPanelPlacement.h,
+        y: panels['3'].grid.y + newLayout.panels.newPanel.grid.h,
       });
     });
   });
 
   describe('Find top left most open space', () => {
     it('no other panels', () => {
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.findTopLeftMostOpenSpace,
-        { width: 6, height: 6, currentPanels: {} }
-      );
-      expect(newPanelPlacement).toEqual({
-        x: 0,
-        y: 0,
-        w: 6,
-        h: 6,
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.findTopLeftMostOpenSpace, {
+        currentLayout: { panels: {}, sections: {}, pinnedPanels: {} },
+        newPanel: { uuid: 'newPanel', type: 'panelType', grid: { w: 6, h: 6 } },
       });
-      expect(otherPanels).toEqual({});
+      expect(newLayout).toEqual({
+        sections: {},
+        pinnedPanels: {},
+        panels: { newPanel: { type: 'panelType', grid: { x: 0, y: 0, w: 6, h: 6 } } },
+      });
     });
 
     it('top left most space is available', () => {
@@ -142,17 +146,23 @@ describe('new panel placement strategies', () => {
         },
       };
 
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.findTopLeftMostOpenSpace,
-        { width: 6, height: 6, currentPanels: panels }
-      );
-      expect(newPanelPlacement).toEqual({
-        x: 0, // placed in the first available spot
-        y: 0,
-        w: 6,
-        h: 6,
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.findTopLeftMostOpenSpace, {
+        currentLayout: { panels, sections: {}, pinnedPanels: {} },
+        newPanel: { uuid: 'newPanel', type: 'panelType', grid: { w: 6, h: 6 } },
       });
-      expect(otherPanels).toEqual(panels); // other panels don't move with this strategy
+      const expectedPanels = {
+        ...panels, // other panels don't move with this strategy
+        newPanel: {
+          type: 'panelType',
+          grid: {
+            x: 0, // placed in the first available spot
+            y: 0,
+            w: 6,
+            h: 6,
+          },
+        },
+      };
+      expect(newLayout.panels).toEqual(expectedPanels); // other panels don't move with this strategy
     });
 
     it('panel must be pushed down', () => {
@@ -163,22 +173,29 @@ describe('new panel placement strategies', () => {
           type: 'lens',
         },
       };
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.findTopLeftMostOpenSpace,
-        { width: 6, height: 6, currentPanels: panels }
-      );
-      expect(newPanelPlacement).toEqual({
-        x: 0,
-        y: 6,
-        w: 6,
-        h: 6,
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.findTopLeftMostOpenSpace, {
+        currentLayout: { panels, sections: {}, pinnedPanels: {} },
+        newPanel: { uuid: 'newPanel', type: 'panelType', grid: { w: 6, h: 6 } },
       });
-      expect(otherPanels).toEqual(panels); // other panels don't move with this strategy
+      const expectedPanels = {
+        ...panels, // other panels don't move with this strategy
+        newPanel: {
+          type: 'panelType',
+          grid: {
+            x: 0,
+            y: 6, // panel is pushed down
+            w: 6,
+            h: 6,
+          },
+        },
+      };
+      expect(newLayout.panels).toEqual(expectedPanels); // other panels don't move with this strategy
     });
 
     it('ignores panels in other sections', () => {
+      const mockedLayout = getMockLayoutWithSections();
       const panels = {
-        ...getMockLayoutWithSections().panels,
+        ...mockedLayout.panels,
         '1': {
           grid: { x: 0, y: 0, w: 6, h: 100 },
           type: 'lens',
@@ -196,17 +213,30 @@ describe('new panel placement strategies', () => {
           type: 'lens',
         },
       };
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.findTopLeftMostOpenSpace,
-        { width: 6, height: 6, currentPanels: panels, sectionId: 'section1' }
-      );
-      expect(newPanelPlacement).toEqual({
-        x: 0,
-        y: 12, // maxY is 12 for section1; maxY of 100 in section 0 is ignored
-        w: 6,
-        h: 6,
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.findTopLeftMostOpenSpace, {
+        currentLayout: {
+          ...mockedLayout,
+          panels,
+        },
+        newPanel: {
+          uuid: 'newPanel',
+          type: 'panelType',
+          grid: { w: 6, h: 6, sectionId: 'section1' },
+        },
       });
-      expect(otherPanels).toEqual(panels); // other panels don't move with this strategy
+      const expectedPanels = {
+        ...panels, // other panels don't move with this strategy
+        newPanel: {
+          type: 'panelType',
+          grid: {
+            x: 0,
+            y: 12, // maxY is 12 for section1; maxY of 100 in section 0 is ignored
+            w: 6,
+            h: 6,
+          },
+        },
+      };
+      expect(newLayout.panels).toEqual(expectedPanels);
     });
 
     it('place panel beside another panel', () => {
@@ -217,18 +247,26 @@ describe('new panel placement strategies', () => {
           type: 'testPanelType',
         },
       };
-      const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-        PlacementStrategy.findTopLeftMostOpenSpace,
-        { width: 6, height: 6, currentPanels: panels, beside: '3' }
-      );
-      // place beside panel 3
-      expect(newPanelPlacement).toEqual({
-        x: 0,
-        y: 6,
-        w: 6,
-        h: 6,
+
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.findTopLeftMostOpenSpace, {
+        currentLayout: { panels, sections: {}, pinnedPanels: {} },
+        newPanel: { uuid: 'newPanel', type: 'panelType', grid: { w: 6, h: 6 } },
+        beside: '3',
       });
-      expect(otherPanels).toEqual(panels); // other panels don't move with this strategy
+      const expectedPanels = {
+        ...panels, // other panels don't move with this strategy
+        newPanel: {
+          type: 'panelType',
+          grid: {
+            // place beside panel 3
+            x: 0,
+            y: 6,
+            w: 6,
+            h: 6,
+          },
+        },
+      };
+      expect(newLayout.panels).toEqual(expectedPanels);
     });
   });
 });

src/platform/plugins/shared/dashboard/public/dashboard_api/layout_manager/layout_manager.ts
@@ -202,20 +202,20 @@ export function initializeLayoutManager(
       };
     }
     const placementHints = await getPlacementHints(type, serializedState);
-    const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(placementHints.strategy, {
-      currentPanels: layout$.value.panels,
-      height: placementHints.height,
-      width: placementHints.width,
+    const updatedLayout = runPanelPlacementStrategy(placementHints.strategy, {
+      currentLayout: layout$.value,
+      newPanel: {
+        type,
+        uuid,
+        grid: {
+          w: placementHints.width,
+          h: placementHints.height,
+        },
+      },
       beside,
     });
 
-    return {
-      ...layout$.value,
-      panels: {
-        ...otherPanels,
-        [uuid]: { grid: newPanelPlacement, type },
-      },
-    };
+    return updatedLayout;
   };
 
   // --------------------------------------------------------------------------------------
@@ -232,30 +232,36 @@ export function initializeLayoutManager(
       const existingPanel: DashboardLayoutPanel | undefined = layout$.value.panels[uuid];
       const sameType = existingPanel?.type === type;
 
-      const grid = existingPanel
-        ? existingPanel.grid
+      const updatedLayout = existingPanel
+        ? {
+            ...layout$.value,
+            panels: {
+              ...layout$.value.panels,
+              [uuid]: { grid: existingPanel.grid, type },
+            },
+          }
         : runPanelPlacementStrategy(PlacementStrategy.findTopLeftMostOpenSpace, {
-            width: size?.width ?? DEFAULT_PANEL_WIDTH,
-            height: size?.height ?? DEFAULT_PANEL_HEIGHT,
-            currentPanels: layout$.value.panels,
+            currentLayout: layout$.value,
+            newPanel: {
+              uuid,
+              type,
+              grid: {
+                w: size?.width ?? DEFAULT_PANEL_WIDTH,
+                h: size?.height ?? DEFAULT_PANEL_HEIGHT,
+              },
+            },
             /**
              * We can assume that all panels being sent as a single package are related; so,
              * place them close together by grouping them around the first embeddable.
              */
             beside: uuid === first.embeddableId ? undefined : first.embeddableId,
-          }).newPanelPlacement;
+          });
       currentChildState[uuid] = {
         ...(sameType && currentChildState[uuid] ? currentChildState[uuid] : {}),
         ...serializedState,
       };
 
-      layout$.next({
-        ...layout$.value,
-        panels: {
-          ...layout$.value.panels,
-          [uuid]: { grid, type },
-        },
-      });
+      layout$.next(updatedLayout);
     }
     trackPanel.setScrollToPanelId(first.embeddableId);
     trackPanel.setHighlightPanelId(first.embeddableId);
@@ -399,26 +405,20 @@ export function initializeLayoutManager(
 
     currentChildState[uuidOfDuplicate] = serializedState;
 
-    const { newPanelPlacement, otherPanels } = placeClonePanel({
-      width: layoutItemToDuplicate.grid.w,
-      height: layoutItemToDuplicate.grid.h,
-      sectionId: layoutItemToDuplicate.grid.sectionId,
-      currentPanels: layout$.value.panels,
-      placeBesideId: uuidToDuplicate,
-    });
-    layout$.next({
-      ...layout$.value,
-      panels: {
-        ...otherPanels,
-        [uuidOfDuplicate]: {
-          grid: {
-            ...newPanelPlacement,
-            sectionId: layoutItemToDuplicate.grid.sectionId,
-          },
-          type: layoutItemToDuplicate.type,
+    const updatedLayout = placeClonePanel({
+      currentLayout: layout$.value,
+      newPanel: {
+        uuid: uuidOfDuplicate,
+        type: layoutItemToDuplicate.type,
+        grid: {
+          w: layoutItemToDuplicate.grid.w,
+          h: layoutItemToDuplicate.grid.h,
+          sectionId: layoutItemToDuplicate.grid.sectionId,
         },
       },
+      placeBesideId: uuidToDuplicate,
     });
+    layout$.next(updatedLayout);
 
     coreServices.notifications.toasts.addSuccess({
       title: dashboardClonePanelActionStrings.getSuccessMessage(),
@@ -623,27 +623,22 @@ export function initializeLayoutManager(
         for (const panelId of Object.keys(newPinnedPanels)) {
           if (newPinnedPanels[panelId].order > originalOrder) newPinnedPanels[panelId].order--;
         }
-
         // place the new control panel in the top left corner, bumping other panels down as necessary
-        const { newPanelPlacement, otherPanels } = runPanelPlacementStrategy(
-          PlacementStrategy.placeAtTop,
-          {
-            currentPanels: layout$.value.panels,
-            height: 2,
-            width: 12,
-          }
-        );
+        const updatedLayout = runPanelPlacementStrategy(PlacementStrategy.placeAtTop, {
+          currentLayout: layout$.value,
+          newPanel: {
+            uuid,
+            type: panelToUnpin.type,
+            grid: {
+              w: 12,
+              h: 2,
+            },
+          },
+        });
 
         // update the layout with the pinned control removed and added as a panel
         layout$.next({
-          ...layout$.getValue(),
-          panels: {
-            ...otherPanels,
-            [uuid]: {
-              type: panelToUnpin.type,
-              grid: { ...newPanelPlacement },
-            },
-          },
+          ...updatedLayout,
           pinnedPanels: newPinnedPanels,
         });
       },

commit a8b48ce7c06f0424c3d390e39ce234ae2a9f55bb

[Dashboard] Fix adding a panel wiping out a populated collapsible section (#275713)
## Summary

Fixes #275712.

When a dashboard's only panels live inside collapsible sections (i.e.
there are no top-level panels), adding a new panel via navigation (e.g.
"Add to dashboard" from Lens or ES|QL) wiped out the existing section
content.

### Root cause

Incoming embeddables are placed at the top level (no `sectionId`) via
the `findTopLeftMostOpenSpace` panel placement strategy. That strategy
computes `maxY` considering only panels in the **same** section as the
new panel:

- With no top-level panels, nothing matches, so `maxY` stays `-1`.
- The `maxY < 0` "empty grid" branch rebuilt the `panels` map with
**only** the new panel, discarding all section-contained panels.

The other two return branches in the same strategy already spread
`...currentPanels`, which is why the bug did not reproduce when at least
one top-level panel existed above the sections. This PR makes the
empty-grid branch preserve existing panels the same way.

## Test plan

- [x] Added a unit test in `place_new_panel_strategies.test.ts` proving
section-contained panels survive when a top-level panel is placed onto a
section-only dashboard (fails before the fix, passes after).
- [x] Added a regression test in `layout_manager.test.ts` covering the
actual `addIncomingEmbeddables` flow with a section-only dashboard.
- [x] `node scripts/jest` passes for both suites (30 tests).
- [x] Manual: create a dashboard with a single collapsible section
containing one panel, add a panel via Lens/ES|QL, and confirm the
existing panel is preserved.

before:


https://github.com/user-attachments/assets/2a1d3796-4efb-4ffd-952f-0cfdc8248a00


after:


https://github.com/user-attachments/assets/fc6a245a-0545-44c5-b899-6b841ebbfef8

## Considerations
I considered placing the new panel inside the section when the section
is the last content on the dashboard, but that complicates the logic
(we'd have to handle collapsed vs. expanded sections, etc.). So this PR
just fixes the bug and restores the original behavior - this was a
regression from #273089. If anyone wants to change the placement

src/platform/plugins/shared/dashboard/public/panel_placement/place_new_panel_strategies.ts
@@ -78,11 +78,12 @@ export const runPanelPlacementStrategy = (
         }
       });
 
-      // Handle case of empty grid.
+      // Handle case where top-level grid has no panels (empty grid or section-only dashboard).
       if (maxY < 0) {
         return {
           ...currentLayout,
           panels: {
+            ...currentPanels,
             [newPanel.uuid]: { type: newPanel.type, grid: { x: 0, y: 0, w: width, h: height } },
           },
         };

src/platform/plugins/shared/dashboard/public/panel_placement/place_new_panel_strategies.test.ts
@@ -239,6 +239,37 @@ describe('new panel placement strategies', () => {
       expect(newLayout.panels).toEqual(expectedPanels);
     });
 
+    it('preserves section panels when placing a top-level panel on a section-only dashboard', () => {
+      // Dashboard where every existing panel lives inside a section (no top-level panels).
+      const currentLayout: DashboardLayout = {
+        panels: {
+          '1': {
+            grid: { x: 0, y: 0, w: 6, h: 6, sectionId: 'section1' },
+            type: 'testPanelType',
+          },
+        },
+        sections: {
+          section1: { grid: { y: 0 }, title: 'Section One', collapsed: false },
+        },
+        pinnedPanels: {},
+      };
+
+      const newLayout = runPanelPlacementStrategy(PlacementStrategy.findTopLeftMostOpenSpace, {
+        currentLayout,
+        // Incoming embeddables are placed at the top level (no sectionId).
+        newPanel: { uuid: 'newPanel', type: 'panelType', grid: { w: 6, h: 6 } },
+      });
+
+      expect(newLayout.panels).toEqual({
+        '1': {
+          grid: { x: 0, y: 0, w: 6, h: 6, sectionId: 'section1' },
+          type: 'testPanelType',
+        },
+        newPanel: { type: 'panelType', grid: { x: 0, y: 0, w: 6, h: 6 } },
+      });
+      expect(newLayout.sections).toEqual(currentLayout.sections);
+    });
+
     it('place panel beside another panel', () => {
       const panels: DashboardLayout['panels'] = {
         ...getMockLayout().panels,