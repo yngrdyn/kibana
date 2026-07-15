74056169c03a0f93947883a306a63b0e051e13e4
Brad White <Ikuni17@users.noreply.github.com>
2026-07-14 19:28:29 -0600

    skip failing suite (#261563)

--- x-pack/platform/test/functional/apps/aiops/log_rate_analysis.ts
+++ x-pack/platform/test/functional/apps/aiops/log_rate_analysis.ts
@@ -323,7 +323,8 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   // Failing: See https://github.com/elastic/kibana/issues/278230
   describe.skip('log rate analysis', function () {
     for (const testData of logRateAnalysisTestData) {
-      describe(`with '${testData.sourceIndexOrSavedSearch}'`, function () {
+      // Failing: See https://github.com/elastic/kibana/issues/261563
+      describe.skip(`with '${testData.sourceIndexOrSavedSearch}'`, function () {
         before(async () => {
           await aiops.logRateAnalysisDataGenerator.generateData(testData.dataGenerator);
 
